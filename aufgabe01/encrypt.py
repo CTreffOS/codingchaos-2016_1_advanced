12227d863c398eda64c281352db558b6acb7d69b
Sebastian Höffner <shoeffner@uos.de>
2016-01-14 18:46:55 +0100

    aufgabe01

--- aufgabe01/encrypt.py
+++ aufgabe01/encrypt.py
@@ -2,8 +2,8 @@ import sys
 import string
 
 key = [ord(i)-97 for i in 'chaostreffosnabrueck']
-word = sys.stdin.readline()[0:1]
+word = sys.stdin.readline()[0:-1]
 val = [ord(i)-97 for i in word]
 out = [string.ascii_lowercase[(key[i%len(key)]+val[i])%26] for i in range(len(word))]
 
-print(out)
+print("".join(out))
