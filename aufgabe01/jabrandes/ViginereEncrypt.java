dd964a8b28877217a1fcc3b4f012875e83d0e01d
Jan-Niklas Brandes <janniklasbrandes@aol.de>
2016-01-14 18:40:54 +0100

    Jetzt aber richtig 2

--- aufgabe01/jabrandes/ViginereEncrypt.java
+++ aufgabe01/jabrandes/ViginereEncrypt.java
@@ -12,7 +12,7 @@ public class ViginereEncrypt {
 		int asciiLength = ((int) 'z') - asciiStart + 1;
 		for (char c : text.toCharArray()) {
 			if (c >= asciiStart && c < asciiStart + asciiLength) {
-				result += (char) (((((int) c - asciiStart) + ((int) Main.key.charAt(keyPos) - asciiStart)) % asciiLength) + asciiStart);
+				result += (char) (((((int) c - asciiStart) + ((int) KEY.charAt(keyPos) - asciiStart)) % asciiLength) + asciiStart);
 				keyPos = (keyPos + 1) % KEY.length();
 			} else {
 				result += c;
